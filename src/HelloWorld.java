b74beedab9dc56fb117c0bcb5ce4dd9997c9043d
Dan <danielandrewvoss@gmail.com>
2016-05-23 10:19:05 -0400

    Add HashMap exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Dan on 5/18/16.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+
+        String name = names.get(0);
+
+        HashMap<String, Integer> scores = new HashMap<>();
+        scores.put("Alice", 10);
+        scores.put("Bob", 7);
+        scores.put("Charlie", 3);
+
+        int score = scores.get("Bob");
+
+        int a = Integer.parseInt("17");
     }
 }
