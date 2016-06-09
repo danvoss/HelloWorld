be6be49fae1b312a4cd68fd082c7ea4dd2391f02
Dan <danielandrewvoss@gmail.com>
2016-06-09 10:20:03 -0400

    Add Exercise 5

---
+++ src/Exercise05.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by Dan on 6/9/16.
+ */
+public class Exercise05 {
+    public static void main(String[] args) {
+        String[] names = {"Charlie", "David", "Alice", "Bob", "Albert"};
+
+        // ArrayList<String> sortedNames
+        // a naive, inefficient sorting algorithm:
+
+        ArrayList<String> sortedNames = new ArrayList<>();
+        for (String name : names) {
+            int index = 0;
+            for (String sortedName : sortedNames) {
+                if (name.compareTo(sortedName) > 0) {
+                    index++;
+                }
+                else {
+                    break;
+                }
+            }
+            sortedNames.add(index, name);
+        }
+    }
+}
