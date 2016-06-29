380e37a252b795784913b6a9ad682f86c64fdcaf
Dan <danielandrewvoss@gmail.com>
2016-06-29 12:24:04 -0400

    Finish ex 7

--- src/Exercise07.java
+++ src/Exercise07.java
@@ -7,31 +7,35 @@ import java.util.List;
 public class Exercise07 {
     public static void main(String[] args) {
         String s = "racecar";
-        System.out.println(isPalindrome("racecar")); // true
-        System.out.println(isPalindrome("hello")); //false
+        System.out.println(isPal2("racecar")); // true
+        System.out.println(isPal3("hello")); //false
+
     }
 
-    public static boolean isPalindrome(String input) {
-
-        char[] array = input.toCharArray();
-        for (int i = 0; i < array.length; i++) {
-
-
-
-        }
-
-        return false;
-    }
+//    public static boolean isPalindrome(String input) {
+//
+//        char[] array = input.toCharArray();
+//        for (int i = 0; i < array.length; i++) {
+//        }
+//        return false;
+//    }
 
     public static boolean isPal2(String input){
         StringBuilder sb = new StringBuilder(input);
         return (input.equals(sb.reverse().toString()));
     }
 
-    public boolean isPal3(String input){
+    public static boolean isPal3(String input){
         int x = 0;
         int y = input.length() - 1;
-
-        return false;
+        while (x>0){
+            if(input.toCharArray()[x]!=input.toCharArray()[y]){
+                return false;
+            } else{
+                x--;
+                y++;
+            }
+        }
+        return true;
     }
 }
