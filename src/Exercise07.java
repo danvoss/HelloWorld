2eb0f440feb01601beb4d939e23fd210496a9802
Dan <danielandrewvoss@gmail.com>
2016-08-29 10:56:34 -0400

    Add palindrone/recursion method

--- src/Exercise07.java
+++ src/Exercise07.java
@@ -9,16 +9,9 @@ public class Exercise07 {
         String s = "racecar";
         System.out.println(isPal2("racecar")); // true
         System.out.println(isPal3("hello")); //false
+        System.out.println(isPal("racecar")); //true
 
     }
-
-//    public static boolean isPalindrome(String input) {
-//
-//        char[] array = input.toCharArray();
-//        for (int i = 0; i < array.length; i++) {
-//        }
-//        return false;
-//    }
 
     public static boolean isPal2(String input){
         StringBuilder sb = new StringBuilder(input);
@@ -26,10 +19,10 @@ public class Exercise07 {
     }
 
     public static boolean isPal3(String input){
-        int x = 0;
-        int y = input.length() - 1;
+        int x = input.length() - 1;
+        int y = 0;
         while (x>0){
-            if(input.toCharArray()[x]!=input.toCharArray()[y]){
+            if(input.toCharArray()[x] != input.toCharArray()[y]){
                 return false;
             } else{
                 x--;
@@ -37,5 +30,16 @@ public class Exercise07 {
             }
         }
         return true;
+    }
+
+    // solve using recursion:
+    public static boolean isPal(String input) {
+        if (input.length() <=1) {
+            return true;
+        }
+        if (input.charAt(0) != input.charAt(input.length()-1)) {
+            return false;
+        }
+        return isPal(input.substring(1, input.length()-1));
     }
 }
